648dbe4649701feccf33103565dd77d47c4a0fa0
nada-r <nadarzer@gmail.com>
2023-11-03 23:54:54 +0100

    add forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [formData, setFormData] = useState({
+        nom: '',
+        texte1: '',
+        texte2: '',
+        texte3: '',
+    });
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:5000/data', formData);
+            console.log(response.data);
+        } catch (error) {
+            console.error('Erreur lors de l\'envoi des données', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Formulaire Simple</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="nom"
+                    value={formData.nom}
+                    onChange={handleChange}
+                    placeholder="Nom"
+                    required
+                />
+                <input
+                    type="text"
+                    name="texte1"
+                    value={formData.texte1}
+                    onChange={handleChange}
+                    placeholder="Texte 1"
+                    required
+                />
+                <input
+                    type="text"
+                    name="texte2"
+                    value={formData.texte2}
+                    onChange={handleChange}
+                    placeholder="Texte 2"
+                    required
+                />
+                <input
+                    type="text"
+                    name="texte3"
+                    value={formData.texte3}
+                    onChange={handleChange}
+                    placeholder="Texte 3"
+                    required
+                />
+                <button type="submit">Envoyer</button>
+            </form>
+        </div>
+    );
 }
 
-export default App
+export default App;
+
