78cb48cfae9bbea28b437cf532352ef0aa92dbbf
nada-r <nadarzer@gmail.com>
2023-11-15 21:57:53 +0100

    start modifying expertise inputs into a tweet

--- src/App.jsx
+++ src/App.jsx
@@ -49,79 +49,71 @@ function App() {
                     <input
                         name="Name"
                         id="Name"
+                        className="input-name"
                         placeholder="Enter Name"
                         value={formData.Name}
                         onChange={handleInputChange}
                     />
                 </div>
-                <fieldset className="input-group">
-                    <legend>Your expertise</legend>
-                    <label htmlFor="Expertise1">Expertise 1</label>
-                    <input
-                        name="Expertise1"
-                        id="Expertise1"
-                        placeholder="Enter Expertise 1"
-                        value={formData.Expertise1}
-                        onChange={handleInputChange}
-                    />
-                    <div className="flex-1 ml-2">
-                        <label htmlFor="Know1">Years of Experience</label>
+                <div className="form-row">
+                    <fieldset className="input-group">
+                        <legend>Your expertise</legend>
+                        <label htmlFor="ExpertiseTweet">Present your expertise in a Tweet</label>
+                        <input
+                            name="Expertise1"
+                            id="Expertise1"
+                            placeholder="Enter Expertise 1"
+                            value={formData.Expertise1}
+                            onChange={handleInputChange}
+                        />
+                        <label htmlFor="Expertise2">Expertise 2</label>
+                        <input
+                            name="Expertise2"
+                            id="Expertise2"
+                            placeholder="Enter Expertise 2"
+                            value={formData.Expertise2}
+                            onChange={handleInputChange}
+                        />
+                        <label htmlFor="Expertise3">Expertise 3</label>
+                        <input
+                            name="Expertise3"
+                            id="Expertise3"
+                            placeholder="Enter Expertise 3"
+                            value={formData.Expertise3}
+                            onChange={handleInputChange}
+                        />
+                    </fieldset>
+                    <fieldset className="input-group">
+                        <legend>Questions for Founders</legend>
+                        <label htmlFor="Question1">Question 1</label>
                         <input
                             type="text"
                             name="Know1"
                             id="Know1"
                             placeholder="I would like to discuss about"
-                            value={formData.Experience1}
+                            value={formData.Question1}
                             onChange={handleInputChange}
                         />
-                    </div>
-                    <label htmlFor="Expertise2">Expertise 2</label>
-                    <input
-                        name="Expertise2"
-                        id="Expertise2"
-                        placeholder="Enter Expertise 2"
-                        value={formData.Expertise2}
-                        onChange={handleInputChange}
-                    />
-                    <label htmlFor="Expertise3">Expertise 3</label>
-                    <input
-                        name="Expertise3"
-                        id="Expertise3"
-                        placeholder="Enter Expertise 3"
-                        value={formData.Expertise3}
-                        onChange={handleInputChange}
-                    />
-                </fieldset>
-                <fieldset className="input-group">
-                    <legend>Questions for Founders</legend>
-                    <label htmlFor="Question1">Question 1</label>
-                    <input
-                        type="text"
-                        name="Know1"
-                        id="Know1"
-                        placeholder="I would like to discuss about"
-                        value={formData.Question1}
-                        onChange={handleInputChange}
-                    />
-                    <label htmlFor="Question2">Question 2</label>
-                    <input
-                        type="text"
-                        name="Know2"
-                        id="Know2"
-                        placeholder="Also I'm looking for"
-                        value={formData.Question2}
-                        onChange={handleInputChange}
-                    />
-                    <label htmlFor="Question3">Question 3</label>
-                    <input
-                        type="text"
-                        name="Know3"
-                        id="Know3"
-                        placeholder="And finally I am curious about"
-                        value={formData.Question3}
-                        onChange={handleInputChange}
-                    />
-                </fieldset>
+                        <label htmlFor="Question2">Question 2</label>
+                        <input
+                            type="text"
+                            name="Know2"
+                            id="Know2"
+                            placeholder="Also I'm looking for"
+                            value={formData.Question2}
+                            onChange={handleInputChange}
+                        />
+                        <label htmlFor="Question3">Question 3</label>
+                        <input
+                            type="text"
+                            name="Know3"
+                            id="Know3"
+                            placeholder="And finally I am curious about"
+                            value={formData.Question3}
+                            onChange={handleInputChange}
+                        />
+                    </fieldset>
+                </div>
                 <button className="button" type="submit">Send</button>
             </form> {/* Close the form tag */}
         </div>
